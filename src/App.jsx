8428a33ef48ca34178c3f8f817deffeb26317ac8
candra kriswinarto <ckriswinarto@gmail.com>
2022-06-07 17:28:01 +0800

    create search ui

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import InputSearch from "./components/InputSearch"
+
+function App() {
+
+  return (
+    <div className="max-w-4xl mx-auto mt-6 px-4">
+      <InputSearch />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/InputSearch.jsx
@@ -0,0 +1,14 @@
+
+const InputSearch = () => {
+  return (
+    <div>
+      <input
+        type="search"
+        placeholder="Search..."
+        className="w-full px-4 py-2 border-gray-500 border-2 rounded-md outline-none"
+      />
+    </div>
+  )
+}
+
+export default InputSearch
